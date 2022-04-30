7893538bf891e65684c7b715eea93e98c4aa7a0d
Selim96 <93911103+Selim96@users.noreply.github.com>
2022-04-30 12:54:27 +0000

    Deploying to gh-pages from @ Selim96/goit-react-hw-05-movies@eaa76b7e6424525553cd472630370411f04f50f3 🚀

---
+++ static/js/234.3d87bdf8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[234],{234:function(e,r,t){t.r(r),t.d(r,{default:function(){return a}});var n={},s=t(184);function a(e){var r=e.reviews;return(0,s.jsx)("ul",{className:n.reviewsList,children:r.map((function(e){var r=e.author,t=e.content;return(0,s.jsxs)("li",{className:n.reviewItem,children:[(0,s.jsxs)("h4",{children:["Author: ",r]}),(0,s.jsx)("p",{children:t})]},r)}))})}}}]);
+//# sourceMappingURL=234.3d87bdf8.chunk.js.map
---
+++ static/js/357.9be9d04b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[357],{357:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184);function u(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h2",{children:"404 View is not found"})})}}}]);
+//# sourceMappingURL=357.9be9d04b.chunk.js.map
